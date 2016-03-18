fcf5e980728c6a9d5bedac45920b853651115386
Tetsuya Mori <t2y3141592@gmail.com>
2016-03-18 10:31:44 +0900

    test: Try to patch web-component-tester-istanbul

---
+++ test/plugin.js
@@ -0,0 +1,42 @@
+var middleware = require('./middleware');
+var istanbul = require('istanbul');
+var Validator = require('./validator');
+var sync = true;
+
+/**
+* Tracks coverage objects and writes results by listening to events
+* emitted from wct test runner.
+*/
+
+function Listener(emitter, pluginOptions) {
+
+  this.options = pluginOptions;
+  this.collector = new istanbul.Collector();
+  this.reporter = new istanbul.Reporter(false, this.options.dir);
+  this.validator = new Validator(this.options.thresholds);
+  this.reporter.addAll(this.options.reporters)
+
+  emitter.on('sub-suite-end', function(browser, data) {
+    if (data && data.__coverage__) {
+      this.collector.add(data.__coverage__);
+    }
+  }.bind(this));
+
+  emitter.on('run-end', function(error) {
+    if (!error) {
+      this.reporter.write(this.collector, sync, function() {});
+
+      if (!validator.validate(this.collector)) {
+        throw new Error('Coverage failed');
+      }
+    }
+  }.bind(this));
+
+  emitter.hook('prepare:webserver', function(express, done){
+    express.use(middleware(emitter.options.root, this.options, emitter));
+    done();
+  }.bind(this));
+
+};
+
+module.exports = Listener;
