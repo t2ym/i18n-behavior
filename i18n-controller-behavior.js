edb4a7d1571914d7317e09bd9db360a6730dbdc2
Tetsuya Mori <t2y3141592@gmail.com>
2019-02-07 17:53:01 +0900

    Issue #67 Export behaviors from i18n-controller-behavior.js

--- i18n-controller-behavior.js
+++ i18n-controller-behavior.js
@@ -2,4 +2,4 @@
 @license https://github.com/t2ym/i18n-behavior/blob/master/LICENSE.md
 Copyright (c) 2016, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
 */
-import "./i18n-behavior.js";
+export { I18nBehavior, _I18nBehavior, I18nControllerBehavior } from "./i18n-behavior.js";
